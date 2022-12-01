cd7e41ba28ee9bfac9c503f2656dfff48ddeaa81
Pedro Lima <emailpedrolima@gmail.com>
2022-12-01 09:50:50 -0300

    notification app

---
+++ src/app/components/notification-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NOTIFICATIONS } from 'src/app/mockts/notifications';
+import { Notification } from 'src/app/models/notification';
+
+@Component({
+  selector: 'ntap-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.scss']
+})
+export class NotificationListComponent {
+  notificacoes: Notification[] = NOTIFICATIONS;
+
+  lerNotificacao(item: Notification): void {
+    item.lido = true;
+  }
+}
